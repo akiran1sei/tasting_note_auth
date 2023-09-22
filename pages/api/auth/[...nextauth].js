7d804e8a3b01424f52e4ad9d8d7a7bd088791de3
akiran1sei <nakamori.1224@gmail.com>
2023-09-22 12:53:40 +0900

    🎨 ログイン認証時の認証確認コード（毎回ログイン時）

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,25 +5,55 @@ import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 // import EmailProvider from "next-auth/providers/email";
 // import nodemailer from "nodemailer";
+
 export default NextAuth({
   providers: [
     // OAuth authentication providers...
     AppleProvider({
       clientId: process.env.APPLE_ID,
       clientSecret: process.env.APPLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
       clientSecret: process.env.FACEBOOK_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
 
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+      // allowDangerousEmailAccountLinking: true,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
     // Passwordless / email sign in
     // EmailProvider({
@@ -39,12 +69,91 @@ export default NextAuth({
     //   from: process.env.EMAIL_FROM,
     // }),
   ],
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error", // Error code passed in query string as ?error=
+    verifyRequest: "/auth/verify-request", // (used for check email message)
+    newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
+
   // callbacks: {
-  //   async signIn({ account, profile }) {
-  //     if (account.provider === "google") {
-  //       return profile.email_verified && profile.email.endsWith("@example.com");
+  //   async signIn({ user, account, profile, email, credentials }) {
+  //     const isAllowedToSignIn = true;
+  //     if (isAllowedToSignIn) {
+  //       return true;
+  //     } else {
+  //       // Return false to display a default error message
+  //       return false;
+  //       // Or you can return a URL to redirect to:
+  //       // return '/unauthorized'
   //     }
-  //     return true; // Do different verification for other providers that don't have `email_verified`
+  //   },
+  //   async redirect({ url, baseUrl }) {
+  //     // Allows relative callback URLs
+  //     if (url.startsWith("/")) return `${baseUrl}${url}`;
+  //     // Allows callback URLs on the same origin
+  //     else if (new URL(url).origin === baseUrl) return url;
+  //     return baseUrl;
+  //   },
+  //   async session({ session, token, user }) {
+  //     // Send properties to the client, like an access_token and user id from a provider.
+  //     session.accessToken = token.accessToken;
+  //     session.user.id = token.id;
+
+  //     return session;
+  //   },
+  //   async jwt({ token, account, profile, isNewUser }) {
+  //     // Persist the OAuth access_token and or the user id to the token right after signin
+  //     if (account) {
+  //       token.accessToken = account.access_token;
+  //       token.id = profile.id;
+  //     }
+  //     return token;
+  //   },
+  // },
+
+  // session: {
+  //   // Choose how you want to save the user session.
+  //   // The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
+  //   // If you use an `adapter` however, we default it to `"database"` instead.
+  //   // You can still force a JWT session by explicitly defining `"jwt"`.
+  //   // When using `"database"`, the session cookie will only contain a `sessionToken` value,
+  //   // which is used to look up the session in the database.
+  //   strategy: "database",
+
+  //   // Seconds - How long until an idle session expires and is no longer valid.
+  //   maxAge: 30 * 24 * 60 * 60, // 30 days
+
+  //   // Seconds - Throttle how frequently to write to database to extend a session.
+  //   // Use it to limit write operations. Set to 0 to always update the database.
+  //   // Note: This option is ignored if using JSON Web Tokens
+  //   updateAge: 24 * 60 * 60, // 24 hours
+
+  //   // The session token is usually either a random UUID or string, however if you
+  //   // need a more customized session token string, you can define your own generate function.
+  //   generateSessionToken: () => {
+  //     return randomUUID?.() ?? randomBytes(32).toString("hex");
+  //   },
+  // },
+  // events: {
+  //   async signIn(message) {
+  //     /* on successful sign in */
+  //   },
+  //   async signOut(message) {
+  //     /* on signout */
+  //   },
+  //   async createUser(message) {
+  //     /* user created */
+  //   },
+  //   async updateUser(message) {
+  //     /* user updated - e.g. their email was verified */
+  //   },
+  //   async linkAccount(message) {
+  //     /* account (e.g. Twitter) linked to a user */
+  //   },
+  //   async session(message) {
+  //     /* session is active */
   //   },
   // },
 });
