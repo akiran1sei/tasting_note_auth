d7d335b17feea99c280f525f0e8106540e932354
akiran1sei <nakamori.1224@gmail.com>
2023-09-25 12:41:15 +0900

    不要なコードの削除

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import AppleProvider from "next-auth/providers/apple";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
+import DiscordProvider from "next-auth/providers/discord";
 // import EmailProvider from "next-auth/providers/email";
 // import nodemailer from "nodemailer";
 
@@ -55,9 +56,13 @@ export default NextAuth({
         },
       },
     }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_ID,
+      clientSecret: process.env.DISCORD_SECRET,
+    }),
+
     // Passwordless / email sign in
     // EmailProvider({
-    //   // server: process.env.EMAIL_SERVER,
     //   server: {
     //     host: process.env.EMAIL_SERVER_HOST,
     //     port: process.env.EMAIL_SERVER_PORT,
@@ -76,84 +81,9 @@ export default NextAuth({
     verifyRequest: "/auth/verify-request", // (used for check email message)
     newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
   },
-
-  // callbacks: {
-  //   async signIn({ user, account, profile, email, credentials }) {
-  //     const isAllowedToSignIn = true;
-  //     if (isAllowedToSignIn) {
-  //       return true;
-  //     } else {
-  //       // Return false to display a default error message
-  //       return false;
-  //       // Or you can return a URL to redirect to:
-  //       // return '/unauthorized'
-  //     }
-  //   },
-  //   async redirect({ url, baseUrl }) {
-  //     // Allows relative callback URLs
-  //     if (url.startsWith("/")) return `${baseUrl}${url}`;
-  //     // Allows callback URLs on the same origin
-  //     else if (new URL(url).origin === baseUrl) return url;
-  //     return baseUrl;
-  //   },
-  //   async session({ session, token, user }) {
-  //     // Send properties to the client, like an access_token and user id from a provider.
-  //     session.accessToken = token.accessToken;
-  //     session.user.id = token.id;
-
-  //     return session;
-  //   },
-  //   async jwt({ token, account, profile, isNewUser }) {
-  //     // Persist the OAuth access_token and or the user id to the token right after signin
-  //     if (account) {
-  //       token.accessToken = account.access_token;
-  //       token.id = profile.id;
-  //     }
-  //     return token;
-  //   },
-  // },
-
-  // session: {
-  //   // Choose how you want to save the user session.
-  //   // The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
-  //   // If you use an `adapter` however, we default it to `"database"` instead.
-  //   // You can still force a JWT session by explicitly defining `"jwt"`.
-  //   // When using `"database"`, the session cookie will only contain a `sessionToken` value,
-  //   // which is used to look up the session in the database.
-  //   strategy: "database",
-
-  //   // Seconds - How long until an idle session expires and is no longer valid.
-  //   maxAge: 30 * 24 * 60 * 60, // 30 days
-
-  //   // Seconds - Throttle how frequently to write to database to extend a session.
-  //   // Use it to limit write operations. Set to 0 to always update the database.
-  //   // Note: This option is ignored if using JSON Web Tokens
-  //   updateAge: 24 * 60 * 60, // 24 hours
-
-  //   // The session token is usually either a random UUID or string, however if you
-  //   // need a more customized session token string, you can define your own generate function.
-  //   generateSessionToken: () => {
-  //     return randomUUID?.() ?? randomBytes(32).toString("hex");
-  //   },
-  // },
-  // events: {
-  //   async signIn(message) {
-  //     /* on successful sign in */
-  //   },
-  //   async signOut(message) {
-  //     /* on signout */
-  //   },
-  //   async createUser(message) {
-  //     /* user created */
-  //   },
-  //   async updateUser(message) {
-  //     /* user updated - e.g. their email was verified */
-  //   },
-  //   async linkAccount(message) {
-  //     /* account (e.g. Twitter) linked to a user */
-  //   },
-  //   async session(message) {
-  //     /* session is active */
-  //   },
-  // },
+  session: {
+    // セッションの有効期限を設定します（ここでは1時間）
+    // maxAge: 1 * 60 * 60, // 1時間
+    maxAge: 10 * 60, // セッションは10分後にタイムアウトします
+  },
 });
